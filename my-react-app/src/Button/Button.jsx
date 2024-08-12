03534127e3c79209f9947a13776d194201181e2c
PNixClaire <phoenix_decastro@dlsu.edu.ph>
2024-08-12 20:00:23 +0800

    inline styling

--- my-react-app/src/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './Button.module.css' //import the css style sheet 
-
-function Button() {
-                            //access the style
-    return (<button className={styles.button}> Click me! </button>); 
-}
-
-export default Button
