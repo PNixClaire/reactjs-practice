405be05f36977e7c5a9260cad40f9513952a435b
PNixClaire <phoenix_decastro@dlsu.edu.ph>
2024-08-12 20:32:17 +0800

    using props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,19 @@
-import Button from "./Button.jsx"
+import Student from "./Student.jsx";
 
 function App() {
-  return (<Button />);
+  return (
+    //student is the child and App is the parent
+     <>
+      {/* now you can reuse the same component but with different data */}
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+
+      {/* this doesn't any props, it uses the default values instead*/}
+      <Student/>
+    </>
+  );
 }
 
 export default App
