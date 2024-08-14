02b619a1ea9539a948131cb5c2abace99fc7d315
PNixClaire <phoenix_decastro@dlsu.edu.ph>
2024-08-14 17:08:20 +0800

    color picker app

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,8 @@
-import MyComponent from "./MyComponent.jsx";
-
-// import Counter from "./Counter.jsx";
+import ColorPicker from "./ColorPicker.jsx";
 
 function App() {
   return (
-    <MyComponent/>
+    <ColorPicker/>
   );
 }
 
